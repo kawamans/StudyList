c7076194fb905ca31d65b808175a7ecf0cd30b44
kawamitsu <intecs.2509ap.02@gmail.com>
2025-12-22 15:23:22 +0900

    12/22 フォワード、リダイレクト

---
+++ study-logs/java/webWork/Redirect/src/main/java/jp/co/seminar/LoginServlet.java
@@ -0,0 +1,37 @@
+package jp.co.seminar;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse; 
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    //正しいログイン情報を定数としておく
+    private static final String UID = "admin";
+    private static final String PWD = "password";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //リクエストで受信した文字をUTF-8文字コードで受信する
+        request.setCharacterEncoding("UTF-8");
+        //データ受信
+        String uid = request.getParameter("uid");
+        String pwd = request.getParameter("pwd");
+        //遷移先ページ格納変数
+        String nextPage;
+        //判定
+        if(uid.equals(UID) && pwd.equals(PWD)) {
+            //ログイン成功時
+            nextPage = request.getContextPath() + "/ok.jsp";
+        } else {
+            //ログイン失敗
+            nextPage = request.getContextPath() + "/ng.jsp";
+        }
+        response.sendRedirect(nextPage);
+        return;
+    }
+}
